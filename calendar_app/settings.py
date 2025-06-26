613589857cccb8659d58d9e6c90eca3f0aa88d5b
Rony George <ronygeorgengit@gmail.com>
2025-06-26 22:12:55 +0530

    added allowed host

--- calendar_app/settings.py
+++ calendar_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.148.233.108']
 
 
 # Application definition
