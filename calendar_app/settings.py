9b642da43d60790be6bbe37b4184570e24784096
Rony George <ronygeorgengit@gmail.com>
2025-06-26 22:23:01 +0530

    req updated

--- calendar_app/settings.py
+++ calendar_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.148.233.108']
+ALLOWED_HOSTS = ['3.148.233.108', '127.0.0.1', '127.0.0.1:8000']
 
 
 # Application definition
